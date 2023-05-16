f10fe55d52e5fd195ffb2f3c85ec6ebfd5d39894
lucas-dem <lucasd393939@gmail.com>
2023-05-16 19:06:09 -0300

    Cambios en visuales

--- front/src/App.js
+++ front/src/App.js
@@ -1,65 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import './App.css'; // Import the CSS file
 import backgroundImage from './images/bathroom brothers.jpeg';
 
 function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [bars, setBars] = useState(['']);
+  const fileInputRef = useRef(null); // Create a ref for the file input
 
-  const handleSelectFile = (event) => {
+  const handleSelectFile = () => {
+    fileInputRef.current.click(); // Programmatically trigger the click event of the file input
+  };
+
+  const handleFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedImage(URL.createObjectURL(file));
   };
 
+  // Event handler for changing the input values
   const handleBarChange = (index, value) => {
     const newBars = [...bars];
     newBars[index] = value;
     setBars(newBars);
+    // Add a new input field if the current one is the last and not empty
     if (index === newBars.length - 1 && value.length > 0 && newBars.length < 6) {
       setBars([...newBars, '']);
     }
   };
 
-  return (
-    <div
-      className="App"
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        padding: '20px',
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-      }}
-    >
-      <div style={{ marginRight: '20px' }}>
-        <div
-          style={{
-            width: '300px',
-            height: '200px',
-            border: '1px solid #ccc',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginBottom: '10px',
-          }}
-        >
-          {selectedImage ? (
-            <img src={selectedImage} alt="Selected" style={{ maxWidth: '100%', maxHeight: '100%' }} />
-          ) : (
-            <p>Select an image</p>
+return (
+    <div className="App">
+      <h1 className="title">Create your NFT</h1>
+      <div className="content">
+        <div className="image-container" onClick={handleSelectFile}>
+          {selectedImage && (
+            <img src={selectedImage} alt="Selected" className="selected-image" />
           )}
         </div>
-        <input type="file" onChange={handleSelectFile} style={{ marginBottom: '10px' }} />
+        <input
+          type="file"
+          onChange={handleFileChange}
+          ref={fileInputRef}
+          className="file-input"
+        />
       </div>
-      <div>
+      <div className="input-container">
         {bars.map((bar, index) => (
-          <div key={index} style={{ marginBottom: '10px' }}>
-            <label>{`Property ${index + 1}:`}</label>
+          <div key={index} className="input-row">
+            <label className="input-label">{`Property ${index + 1}:`}</label> {/* Label for the input field */}
             <input
               value={bar}
               onChange={(event) => handleBarChange(index, event.target.value)}
               maxLength={index === bars.length - 1 ? 10 : undefined} // Set the maximum length to 10 for the last input field
+              className="input-field"
             />
           </div>
         ))}
