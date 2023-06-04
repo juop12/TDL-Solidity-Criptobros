8940f844c77a84a1d5f85b3a69b81298ab557d25
Eliana Harriet <eharriet@fi.uba.ar>
2023-06-04 15:53:10 -0300

    modifiers

--- Contracts/contracts/auction.sol
+++ Contracts/contracts/auction.sol
@@ -3,15 +3,18 @@
 pragma solidity ^0.8.9;
 // Indica la versión del compilador de Solidity que se utilizará.
 
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
 import "./erc1155.sol";
 
 contract Auction is ERC1155Holder {
+    using SafeMath for uint256;
     address public creator;         // La dirección del creador de la subasta
-    uint256 public directBuyPrice;  // El precio de compra directa
     uint256 public startPrice;      // Precio inicial de la subasta
+    uint256 public minIncrement;    // Incremento mínimo de la puja
+    uint256 public directBuyPrice;  // El precio de compra directa
 
     IERC1155 nft1155;               // El token NFT
     uint256 public tokenId;         // El id del token
@@ -26,18 +29,46 @@ contract Auction is ERC1155Holder {
 
     MyToken smartContract;          // El contrato inteligente de MyToken
 
-
-    enum AuctionState {
-        OPEN,
-        CANCELLED,
-        ENDED,
-        DIRECT_BUY,
-        ENDED_BY_CREATOR
-    }
+    event NewBid(address indexed _from, uint256 _bid);
+    event AuctionEnded(address indexed _winner, uint256 _amount);
+    event FundsCollected(address indexed _creator, uint256 _amount);
+    event NFTCollected(address indexed _winner);
 
     struct Bid {
         address sender;
         uint256 bid;
+    }
+
+    modifier open() {
+        require(block.timestamp < endTime, "The auction has already ended");
+        _;
+    }
+
+    modifier ended() {
+        require(block.timestamp >= endTime, "The auction has not ended yet");
+        _;
+    }
+
+    modifier onlyCreator() {
+        require(msg.sender == creator, "Only the creator can collect the funds");
+        _;
+    }
+
+    modifier notCreator() {
+        require(msg.sender != creator, "The creator cannot bid");
+        _;
+    }
+
+    modifier maxBidder() {
+        require(msg.sender == maxBidder, "Only the highest bidder can only collect the token");
+        _;
+    }
+
+    modifier validBid() {
+        require(msg.value > startPrice, "The bid is lower than the start price");
+        require(msg.value > maxBid, "There already is a higher bid");
+        require(msg.value >= maxBid + minIncrement, "The bid has to set a significant increase");
+        _;
     }
 
     constructor(
@@ -76,73 +107,83 @@ contract Auction is ERC1155Holder {
         return (addrs, bidPrice);
     }
 
-    // Crea una nueva puja
-    function placeBid(address bidder, uint256 bidAmmount) external payable returns (bool) {
+    // // Crea una nueva puja
+    // function placeBid(address bidder, uint256 bidAmmount) external payable returns (bool) {
         
-        require(getAuctionState() == AuctionState.OPEN, "The auction is not open");
-        require(block.timestamp < endTime, "The auction has already ended");
-        require(bidAmmount > startPrice, "The bid is lower than the start price");
-        require(bidAmmount > maxBid, "There already is a higher bid");
+    //     require(bidder != creator, "The creator cannot bid");
+    //     require(block.timestamp < endTime, "The auction has already ended");
+    //     require(bidAmmount > startPrice, "The bid is lower than the start price");
+    //     require(bidAmmount > maxBid, "There already is a higher bid");
+    //     require(bidAmmount >= maxBid + minIncrement, "The bid is lower than the minimum increment");
 
-        maxBid = bidAmmount;
-        maxBidder = bidder;
+    //     if (bidder == maxBidder) {
 
-        bids.push(Bid(bidder, bidAmmount));
+    //         bids[bids.length - 1].bid = maxBid;
+
+    //     } else {
+    //         bids.push(Bid(bidder, bidAmmount));
+            
+    //         if (maxBidder != address(0)) {
+    //             smartContract.transfer(maxBidder, maxBid);
+    //         }
+
+    //         maxBid = bidAmmount;
+    //         maxBidder = bidder;
+    //     }
+
+    //     if (bidAmmount >= directBuyPrice) {
+    //         endTime = block.timestamp;
+    //     }
+    //     return true;
+    // }
+
+    // Crea una nueva puja (usa los modificadores)
+    function placeBid() external payable open notCreator validBid returns (bool) {
+
+        if (msg.value >= directBuyPrice) {
+            endTime = block.timestamp;
+        }
+
+        if (msg.sender == maxBidder) {
+            bids[bids.length - 1].bid = msg.value;
+
+            return true;
+        }
+
+        if (maxBidder != address(0)) {
+            smartContract.transfer(maxBidder, maxBid);
+        }
+
+        maxBid = msg.value;
+        maxBidder = msg.sender;
+        bids.push(Bid(msg.sender, msg.value));
 
         return true;
     }
 
     // El postor máximo obtiene el token
-    function collectNFT() external returns (bool) {
+    function collectNFT() external ended maxBidder returns (bool) {
 
-        require(getAuctionState() == AuctionState.ENDED || getAuctionState() == AuctionState.DIRECT_BUY || getAuctionState() == AuctionState.ENDED_BY_CREATOR, "The auction has not ended yet");
-        
-        require(msg.sender == maxBidder, "Only the highest bidder can only collect the token");
-        
         nft1155.safeTransferFrom(address(this), maxBidder, tokenId, nftAmount, "");
         return true;
     }
 
     // El creador obtiene el dinero
-    function collectFunds() external returns (bool) {
-
-        require(getAuctionState() == AuctionState.ENDED || getAuctionState() == AuctionState.DIRECT_BUY || getAuctionState() == AuctionState.ENDED_BY_CREATOR, "The auction has not ended yet");
+    function collectFunds() external ended onlyCreator returns (bool) {
         
-        require(msg.sender == creator, "Only the creator can collect the funds");
-
-
-        //  averiguar: como transfiero plata usando nuestro contrato!
+        //  averiguar: como transfiero plata usando nuestro contrato! (ver tema de pagar gas y todo eso)
         smartContract.transfer(creator, maxBid);
-
-        return true;
-    }
-
-    // El creador termina la subasta
-    function creatorEndAution() external returns (bool) {
-
-        require(msg.sender == creator, "Only the creator can end the auction");
-        require(getAuctionState() == AuctionState.OPEN, "The auction is not open");
-
-        endTime = block.timestamp;
         return true;
     }
 
     // Cancela la subasta
-    function cancelAuction() external returns (bool) {
-            
-        require(msg.sender == creator, "Only the creator can cancel the auction");
-        require(getAuctionState() == AuctionState.OPEN, "The auction is not open");
+    function cancelAuction() external open onlyCreator returns (bool) {
 
         endTime = block.timestamp;
 
-        nft1155.safeTransferFrom(address(this), maxBidder, tokenId, nftAmount, ""); // Transfer the token to the highest bidder
-        smartContract.transfer(maxBidder, maxBid);
-        return true;
-    }
+        nft1155.safeTransferFrom(address(this), creator, tokenId, nftAmount, ""); // Transfer the token to the highest bidder
+        smartContract.transfer(creator, maxBid);
 
-    // Obtiene el estado de la subasta
-    function getAuctionState() public view returns (AuctionState) {
-        if (block.timestamp >= endTime) return AuctionState.ENDED;
-        if (block.timestamp >= startTime) return AuctionState.OPEN;
+        return true;
     }
 }
