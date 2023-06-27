1947891a69f077c16b6dceb00fd47dbad3920b55
lucas-dem <lucasd393939@gmail.com>
2023-06-27 16:15:46 -0300

    cambios auction Co-authored-by: Julián Melmer Stiefkens <juop12@users.noreply.github.com>

--- front/src/Auction.js
+++ front/src/Auction.js
@@ -3,6 +3,7 @@ import './Auction.css';
 import Moralis from 'moralis';
 import console from './lib/console-browserify';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Auction() {
   const [address, setAddress] = useState('');
@@ -35,6 +36,28 @@ function Auction() {
     }
   }, [address]);
 
+  const getUri = async () => {
+    try {
+      const response = await axios.post('https://api.defender.openzeppelin.com/autotasks/d1c6bf30-0112-43a9-9352-bc978df633ab/runs/webhook/16764d23-28d5-46ba-a418-c064f2089339/LT7R5XRK72KshopDm25Cos');
+      console.log('GetUri successfully:', response.data.result);
+      let result = JSON.parse(response.data.result);
+      console.log('Get image:', result.body.message);
+      let url = result.body.message;
+      const responseIpfs = await fetch(url);
+      const data = await responseIpfs.text();
+      console.log(data);
+      /*
+      const trimmedString = responseIpfs.url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('%'));
+      console.log('Get TRIM:', trimmedString);
+      //const trimmedWithoutQuotes = trimmed.replace(/"$/, '');
+      const concatenatedUrl = "https://ipfs.io/ipfs/" + trimmedString;
+      console.log('Get image:', concatenatedUrl);
+      */
+    } catch (error) {
+      console.error('Error minting NFT:', error);
+    }
+  };
+
   const handleButtonClick = () => {
     showTable(false);
     showAuction(true);
@@ -47,6 +70,14 @@ function Auction() {
   return (
     <div className="auction-page">
 
+      <div className="titleAuction">
+        <span className="title-textAuction">Auction</span>
+      </div>
+
+      <div className="image-container">
+          
+      </div>
+
       <div className="center-image">
         {/* Agrega aquí el código para mostrar la imagen en el centro de la página */}
       </div>
@@ -56,25 +87,25 @@ function Auction() {
         {/* Agrega aquí el código para mostrar el máximo postor */}
       </div>
 
-      <div className="address-container"> {/* Add a container for the address input field */}
+      <div className="input-container"> {/* Add a container for the address input field */}
         <input
           value={address}
           onChange={handleAddressChange}
-          className="address-field"
+          className="input-field"
           placeholder="Wallet Address"
         />
       </div>
-      <div className="address-container"> {/* Add a container for the address input field */}
+      <div className="input-container"> {/* Add a container for the address input field */}
         <input
           value={address}
           onChange={handleAddressChange}
-          className="address-field"
+          className="input-field"
           placeholder="Bid Amount"
         />
       </div>
 
       <div className="button-container">
-        <button onClick={handleButtonClick} className="big-button">
+        <button onClick={getUri} className="big-button">
           Bid
         </button>
       </div>
