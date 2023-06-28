585fa7f2a0f55c5ea26be5ebe8c0d7f51a5613c2
lucas-dem <lucasd393939@gmail.com>
2023-06-27 21:56:02 -0300

    agregamos fetch bid arreglos de la imagem

--- front/src/Auction.js
+++ front/src/Auction.js
@@ -1,100 +1,77 @@
 import React, { useEffect, useState } from 'react';
 import './Auction.css';
 import Moralis from 'moralis';
-import console from './lib/console-browserify';
-import { Link, json } from 'react-router-dom';
 import axios from 'axios';
 
 function Auction() {
   const [address, setAddress] = useState('');
-  const [table, showTable] = useState(false);
-  const [auction, showAuction] = useState(false);
-  const [tokens, setTokens] = useState([]);
-  const [responseString, setResponseString] = useState('');
+  const [imageURL, setImageURL] = useState('');
+  const [bid, setBid] = useState('');
 
   useEffect(() => {
-    const fetchTokens = async () => {
+    const fetchImage = async () => {
       try {
-        const response = await Moralis.EvmApi.nft.getWalletNFTs({
-          chain: "0x13881", // Mumbai Testnet
-          format: "decimal",
-          mediaItems: true,
-          address: address
-        });
-
-        setTokens(response.result);
-        setResponseString(JSON.stringify(response, null, 2));
+        const responseImage = await axios.post('https://api.defender.openzeppelin.com/autotasks/d1c6bf30-0112-43a9-9352-bc978df633ab/runs/webhook/16764d23-28d5-46ba-a418-c064f2089339/LT7R5XRK72KshopDm25Cos');
+        const resultImage = JSON.parse(responseImage.data.result);
+        const url = JSON.parse(resultImage.body.message);
+        const responseIpfs = await axios.get(url);
+        const image_url = responseIpfs.data.image;
+        const trimmedString = image_url.split('://')[1];
+        const concatenatedUrl = "https://ipfs.io/ipfs/" + trimmedString;
+        setImageURL(concatenatedUrl);
       } catch (error) {
-        console.error('Error fetching tokens:', error);
-        setTokens([]);
-        setResponseString(`Error fetching tokens: ${error.message}`);
+        console.error('Error getting image:', error);
       }
     };
 
-    if (address) {
-      fetchTokens();
-    }
-  }, [address]);
+    const fetchBid = async () => {
+      try {
+        const responseBid = await axios.post('https://api.defender.openzeppelin.com/autotasks/53727e70-9b63-4c3f-b08f-df347928bea7/runs/webhook/16764d23-28d5-46ba-a418-c064f2089339/MC9XoNL6zhuqeW5fTKFR4p');
+        console.log(responseBid);
+        const resultBid = JSON.parse(responseBid.data.result);
+        const bidObject = JSON.parse(resultBid.body.message);
+        //const bidString = JSON.stringify(bidObject);
+        //setBid(bidString);
+      } catch (error) {
+        console.error('Error getting bid:', error);
+      }
+    };
+    
 
-  
-  const getUri = async () => {
-    try {
-      const response = await axios.post('https://api.defender.openzeppelin.com/autotasks/d1c6bf30-0112-43a9-9352-bc978df633ab/runs/webhook/16764d23-28d5-46ba-a418-c064f2089339/LT7R5XRK72KshopDm25Cos');
-
-      let result = JSON.parse(response.data.result);
-      console.log('GetUri successfully:', result);
-
-      let url = JSON.parse(result.body.message);
-      console.log('Get message:', url);
-
-      let responseIpfs = await axios.get(url)
-      
-      const image_url = responseIpfs.data.image;
-      console.log('Image text:', image_url);
-
-      const trimmedString = image_url.split('://')[1];
-      console.log('Image url:', trimmedString);
-
-      const concatenatedUrl = "https://ipfs.io/ipfs/" + trimmedString;
-      console.log('Get image:', concatenatedUrl);
-
-      return concatenatedUrl
-
-    } catch (error) {
-      console.error('Error minting NFT:', error);
-    }
-  };
-
-  const handleButtonClick = () => {
-    showTable(false);
-    showAuction(true);
-  };
+    fetchImage();
+    fetchBid();
+  }, [bid]);
 
   const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
 
+  const handleBidChange = (event) => {
+    setBid(event.target.value);
   };
 
   return (
     <div className="auction-page">
-
       <div className="titleAuction">
         <span className="title-textAuction">Auction</span>
       </div>
 
-      <div className="image-container">
-          
-      </div>
-
-      <div className="center-image">
-        {/* Agrega aquí el código para mostrar la imagen en el centro de la página */}
+      <div className="image-container-auction">
+        {imageURL && <img src={imageURL} alt="Imagen" />}
       </div>
 
       <div className="max-bidder-display">
         MAX BIDDER:
+        {bid && <span className="max-bidder">{bid}</span>}
+      </div>
+
+      <div className="balance-display">
+        BALANCE:
         {/* Agrega aquí el código para mostrar el máximo postor */}
       </div>
 
-      <div className="input-container"> {/* Add a container for the address input field */}
+      <div className="input-container">
+        {/* Add a container for the address input field */}
         <input
           value={address}
           onChange={handleAddressChange}
@@ -102,17 +79,18 @@ function Auction() {
           placeholder="Wallet Address"
         />
       </div>
-      <div className="input-container"> {/* Add a container for the address input field */}
+      <div className="input-container">
+        {/* Add a container for the address input field */}
         <input
           value={address}
-          onChange={handleAddressChange}
+          onChange={handleBidChange}
           className="input-field"
           placeholder="Bid Amount"
         />
       </div>
 
       <div className="button-container">
-        <button onClick={getUri} className="big-button">
+        <button className="big-button">
           Bid
         </button>
       </div>
